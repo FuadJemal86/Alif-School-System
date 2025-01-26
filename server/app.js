0f3f7ef3b6ce593ba21d39b1309ada575469787f
trip <fuad47722@gmail.com>
2025-01-26 09:47:43 +0300

    Refactor middleware setup in app.js by removing unused packages and reorganizing code structure

--- server/app.js
+++ server/app.js
@@ -3,46 +3,28 @@ const { admin } = require('./Route/admin');
 const { teacher } = require('./Route/teacher');
 const { student } = require('./Route/student');
 const cors = require('cors');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware setup
-app.use(helmet());
 app.use(cors({
     origin: 'https://alif-school-system.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'UPDATE'],
     credentials: true,
 }));
-app.use(morgan('combined'));
-app.use(express.json());
-app.use(express.static('public'));
 
-// Prevent NoSQL injection & XSS
-app.use(mongoSanitize());
 
-// Routes
 app.get("/", (req, res) => {
     res.send("Welcome to Alif API!");
 });
+app.use(express.json());
 app.use('/auth', admin);
 app.use('/teacher', teacher);
 app.use('/student', student);
+app.use(express.static('public'));
 
 
-// Start the server
 app.listen(process.env.PORT || 3032, () => {
     console.log(`Server is running on port ${process.env.PORT || 3032}`);
-});
-
-
-
-
-
-
-
-
-
+}); 
